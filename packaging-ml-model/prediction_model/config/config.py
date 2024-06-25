4eb15c3538d4a671aadbed2e82cba76be1188f7f
Sahil Sharma <47265746+shlsharma@users.noreply.github.com>
2024-06-25 09:02:47 +0000

    ML Module Packaging

---
+++ packaging-ml-model/prediction_model/config/config.py
@@ -0,0 +1,7 @@
+'''
+Any variables that i want to create i will mentioning here
+Also I going to notedown all the path to the directories as well as any intial setting that I want to perform
+For any manual config or any hyper parameter tuning in future
+Treat predicition_model as class or package
+'''
+import pathlib
